0112dd2296fbf13fe4a014cdcb8e5ac461696711
thomascool <thomas.cool@gmail.com>
2015-11-21 13:27:11 -0800

    updates

--- pack_html.sh
+++ pack_html.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 MMDD=`date +%m%d`
+MMDD7=`date -v-7d +%m%d`
 
+rm -rf ./$MMDD7
 mkdir ./html/$MMDD
 mv ./html/*html ./html/$MMDD
-tar -cvzf ../$MMDD.gz ./html/$MMDD
+tar -cvzf ../databackup/$MMDD.gz ./html/$MMDD
 mv ./html/$MMDD .
+
---
+++ weeklydump.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+MMDD=`date +%m%d`
+YYMMDD1=`date -v-1d +%y%m%d`
+YYMMDD2=`date -v-2d +%y%m%d`
+YYMMDD3=`date -v-3d +%y%m%d`
+YYMMDD4=`date -v-4d +%y%m%d`
+YYMMDD0=`date  +%y%m%d`
+
+pg_dump -t 's__*' -t '*__$YYMMDD1' -t '*__$YYMMDD2' -t '*__$YYMMDD3' -t '*__$YYMMDD4' -t '*__$YYMMDD0'  bigoptions | gzip > weekly$MMDD.gz
+
+pg_dump bigoptions | gzip > bigoptions$MMDD.gz
+
