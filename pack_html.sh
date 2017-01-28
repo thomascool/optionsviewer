9f1569bce6f2d11284c36bdf18cdf5dd2d230874
thomascool <thomas.cool@gmail.com>
2017-01-28 15:36:54 -0800

    Updates

--- pack_html.sh
+++ pack_html.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 MMDD=`date +%m%d`
+MMDD1=`date -v-1d +%m%d`
 MMDD7=`date -v-7d +%m%d`
 
+rm -rf ./$MMDD1
 rm -rf ./$MMDD7
 mkdir ./html/$MMDD
 mv ./html/*html ./html/$MMDD
---
+++ td_token.js
@@ -0,0 +1,32 @@
+
+var config = require('config');
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({ show: false});
+
+nightmare
+    .viewport(1000, 1000)
+    .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+    .goto(config.get('tdURL'))
+    .wait()
+    .type('form[class="tdaForm loginForm"] [name=tbUsername]', config.get('tbUsername'))
+    .type('form[class="tdaForm loginForm"] [name=tbPassword]', config.get('tbPassword'))
+    .click('form[class="tdaForm loginForm"] [type=submit]')
+    .wait()
+    //.type('form[class="tdaForm securityChallengeForm"] [name=challengeAnswer]', config.get('challengeAnswer'))
+    //.click('form[class="tdaForm securityChallengeForm"] [type=checkbox]')
+    //.click('form[class="tdaForm securityChallengeForm"] [type=submit]')
+    //.wait()
+    .cookies.get("TDATRADING")
+    .then(function(cookies) {
+        var output = {
+            "webRequest": {
+                "headers": {
+                    "Cookie": "helloWorld"
+                }
+            }
+        };
+        output.webRequest.headers.Cookie = "TDATRADING=" + cookies.value;
+        console.log(JSON.stringify(output));
+        process.exit()
+    });
+
